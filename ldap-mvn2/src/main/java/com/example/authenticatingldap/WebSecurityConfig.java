7dc9424f86398cc6d15d6e416f92ea0fb547cd74
gituser <anujashridhar@gmail.com>
2020-11-09 15:58:39 +0530

    d

--- ldap-mvn2/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
+++ ldap-mvn2/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -9,54 +9,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
 	@Autowired
 	CustomUserContextMapper customUserContextMapper;
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-			.anyRequest().authenticated()
-			.and()
+				.anyRequest().fullyAuthenticated()
+				.and()
 			.formLogin();
 	}
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
-			.ldapAuthentication().userSearchBase("dc=cdac,dc=in")
-				.userDnPatterns("uid={0},ou=Systems")
-			//	.groupSearchBase("ou=groups")
-				.userDetailsContextMapper(customUserContextMapper)
+			.ldapAuthentication()
+			.userDetailsContextMapper(customUserContextMapper)
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
 				.contextSource()
-					.url("ldap://10.224.0.21:389/dc=cdac,dc=in")
-					.managerDn("uid=dhcpdelhi,ou=Systems,dc=cdac,dc=in")
-					.managerPassword("dl@123unified")
-					.port(389);
-					
-					//.and()
-				//.passwordCompare()
+				
+					.url("ldap://localhost:8389/dc=springframework,dc=org")
+					.and()
+				.passwordCompare()
 					//.passwordEncoder(new BCryptPasswordEncoder())
-					//.passwordAttribute("userPassword");
+					.passwordAttribute("userPassword");
 	}
 
-	/*
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth
-        .ldapAuthentication()
-        .userDetailsContextMapper(customUserContextMapper)
-        .userSearchFilter("(uid={0})")
-        .userSearchBase("dc=zflexsoftware,dc=com")
-        .groupSearchFilter("uniqueMember={0}")
-        .groupSearchBase("ou=sysadmins,dc=zflexsoftware,dc=com")
-        .userDnPatterns("uid={0}")
-        .contextSource()
-        .url("ldap://www.zflexldap.com:389")
-        .managerDn("cn=ro_admin,ou=sysadmins,dc=zflexsoftware,dc=com")
-        .managerPassword("zflexpass");
-	}
-*/
 }
