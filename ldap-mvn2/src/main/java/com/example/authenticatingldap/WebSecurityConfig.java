ad77bb74ccd1fd8e1c8a285717889886493ed0cf
gituser <anujashridhar@gmail.com>
2020-11-09 15:58:55 +0530

    e

--- ldap-mvn2/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
+++ ldap-mvn2/src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth
+		
 			.ldapAuthentication()
 			.userDetailsContextMapper(customUserContextMapper)
 				.userDnPatterns("uid={0},ou=people")
